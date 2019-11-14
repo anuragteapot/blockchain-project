63d37b507d6045660604e29acda74c783860718e
anu1601cs <anuragvns1111@gmail.com>
2019-11-14 20:25:47 +0530

    fix and add web3

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import Main from "./components/main";
 import SignUp from "./components/signup";
 import CheckOut from "./components/checkout";
 import { UserProvider } from "./context/userContext";
-import api from "./api";
 import SimpleSnackbar from "./components/SimpleSnackbar";
 import Judge from "./components/judge";
 import Profile from "./components/Profile";
 import { ethers } from "ethers";
+import Web3 from "web3";
+import truffleContract from "truffle-contract";
+import suitList from "./build/contracts/SuitList.json";
 // import NotFound from './components/notfound';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {}
+      storageData: [],
+      web3: null,
+      accounts: null,
+      contract: null
     };
   }
 
@@ -25,31 +30,93 @@ class App extends Component {
     return ethers.utils.formatBytes32String(string);
   }
 
-  async componentDidMount() {
-    const user = await api.GET_USER();
-    if (user) {
-      this.setState({
-        user: user.data
-      });
-    }
-
-    const data = await api.getSuit();
-    console.log(data);
-
-    await api.SuitList.createSuit(
-      this.stoh("ff"),
-      this.stoh("gg"),
-      this.stoh("Anurag content"),
-      this.stoh("anurag"),
-      this.stoh("anurag"),
-      this.stoh("anurag"),
-      this.stoh("anurag"),
-      this.stoh("anurag"),
-      this.stoh("Passed")
-    );
-    console.log(ethers.utils.parseBytes32String(data[0].accusedContent));
+  htos(string) {
+    return ethers.utils.parseBytes32String(string);
   }
 
+  componentDidMount = async () => {
+    try {
+      // Modern dapp browsers...
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+          // Request account access if needed
+          await window.ethereum.enable();
+          // Acccounts now exposed
+          window.web3.eth.sendTransaction({
+            /* ... */
+          });
+        } catch (error) {
+          // User denied account access...
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        // Acccounts always exposed
+        window.web3.eth.sendTransaction({
+          /* ... */
+        });
+      }
+      // Non-dapp browsers...
+      else {
+        console.log(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+      }
+
+      const web3 = window.web3;
+      const accounts = await web3.eth.getAccounts();
+
+      const Contract = truffleContract(suitList);
+      Contract.setProvider(web3.currentProvider);
+      const instance = await Contract.deployed();
+
+      this.setState(
+        {
+          web3,
+          accounts,
+          contract: instance
+        },
+        this.loadData
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  loadData = async () => {
+    const { contract } = this.state;
+
+    const taskCount = await contract.suitCount();
+
+    let result = [];
+    for (let i = 1; i <= taskCount.toNumber(); i++) {
+      const suit = await contract.suits(i);
+      result.push(suit);
+    }
+    this.setState({
+      storageData: result
+    });
+  };
+
+  handleClick = async () => {
+    const { accounts, contract } = this.state;
+    const data = [
+      "1",
+      "2",
+      "Anurag content",
+      "anurag",
+      "anurag",
+      "anurag",
+      "anurag",
+      "anurag",
+      "Passed"
+    ];
+
+    const hexData = data.map(val => this.stoh(val));
+    await contract.createSuit(...hexData, { from: accounts[0] });
+  };
   render() {
     return (
       <div className="App">
--- src/api/index.js
+++ src/api/index.js
@@ -1,77 +1,8 @@
 import AXIOS_API from "./axios";
 import handleError from "./handleError";
-import Web3 from "web3";
-import TruffleContract from "truffle-contract";
-import SuitList from "./../build/contracts/SuitList.json";
 
 class Api {
-  constructor() {
-    this.contracts = {};
-    this.SuitList = null;
-    this.accounts = [];
-  }
-
-  async getSuit() {
-    await this.loadWeb3();
-    await this.loadAccount();
-    await this.loadContract();
-    return await this.loadSuitList();
-  }
-
-  async loadWeb3() {
-    if ("ethereum" in window) {
-      window.web3 = new Web3(window.ethereum);
-      try {
-        // Request account access if needed
-        await window.ethereum.enable();
-        // Acccounts now exposed
-        window.web3.eth.sendTransaction({
-          /* ... */
-        });
-      } catch (error) {
-        // User denied account access...
-      }
-    } else if ("web3" in window) {
-      window.web3Provider = window.web3.currentProvider;
-      window.web3 = new Web3(window.web3.currentProvider);
-      // Acccounts always exposed
-      window.web3.eth.sendTransaction({
-        /* ... */
-      });
-    } else {
-      console.log(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
-    }
-  }
-
-  async loadContract() {
-    if ("web3" in window) {
-      this.contracts.SuitList = TruffleContract(SuitList);
-      this.contracts.SuitList.setProvider(window.web3.currentProvider);
-
-      // Hydrate the smart contract with values from the blockchain
-      this.SuitList = await this.contracts.SuitList.deployed();
-    }
-  }
-
-  async loadAccount() {
-    if ("web3" in window) {
-      // Set the current blockchain account
-      this.accounts = await window.web3.eth.getAccounts();
-      window.web3.eth.defaultAccount = this.accounts[0];
-    }
-  }
-
-  async loadSuitList() {
-    const taskCount = await this.SuitList.suitCount();
-    let result = [];
-    for (let i = 1; i <= taskCount.toNumber(); i++) {
-      const suit = await this.SuitList.suits(i);
-      result.push(suit);
-    }
-    return result;
-  }
+  constructor() {}
 
   async LOGIN(email, password) {
     try {
